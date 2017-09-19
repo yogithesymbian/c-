c1851f65ca91823da5514428bdfcab763e5acaf4
yogithesymbian <yogirenbox33@gmail.com>
2017-09-19 16:48:19 +0800

    Update whilebool.cpp

--- whilebool.cpp
+++ whilebool.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <conio.h>
 using namespace std;
 
 void gotoyx(int x, int y)
@@ -12,8 +13,11 @@ void gotoyx(int x, int y)
 int main()
 {
 	string namayogi;
-	int nimyo, nilaiyog, i=1;
-	
+	int nimyo;
+	double nilaiyog;
+	char anugrade;
+
+	system("Color 0A"); //hapus angka 0 nya gan buat liat color code nya jadi hanya color a saja
 	cout <<"++++++++++++++++++++++++++++++" <<endl;
 	cout <<"     Progam Data Mahasiswa    " <<endl;
 	cout <<"++++++++++++++++++++++++++++++" <<endl;
@@ -23,11 +27,32 @@ int main()
 	cin >>nimyo;
 	cout <<"Masukan Nilai : " ;
 	cin >>nilaiyog;
+	
+	if (nilaiyog >= 85 && nilaiyog <=100)
+	{
+		anugrade = 'A';
+	}
+	else if (nilaiyog >= 70 && nilaiyog <85 )
+	{
+		anugrade = 'B';
+	}
+	else if (nilaiyog > 55 && nilaiyog <70 )
+	{
+		anugrade = 'C';
+	}
+	else if (nilaiyog >= 40 && nilaiyog <55 )
+	{
+		anugrade = 'D';
+	}
+	else{
+		anugrade ='E';
+	}
 	cout <<"++++++DATA MAHASISWA+++++++" << endl;
-	cout <<"Nama    : " <<namayogi <<endl;
-	cout <<"Nim     : " <<nimyo <<endl;
-	cout <<"Nilai   : " <<nilaiyog <<endl;
-
+	cout <<"Nama    	: " <<namayogi <<endl;
+	cout <<"Nim     	: " <<nimyo <<endl;
+	cout <<"Nilai   	: " <<nilaiyog <<endl;
+	cout <<"Nilai Index	: " <<anugrade <<endl;
+//looping with bool .
 bool deloop = true;
 char traegen;
 while (deloop == true)
@@ -42,9 +67,19 @@ while (deloop == true)
 	else if (traegen == 'n')
 	{
 		deloop = false;
-gotoyx(50,24);
-cout <<" Thanks for using Program GNU/GPL " <<endl;
-	system("PAUSE");	
+gotoyx(45,24);
+//dont change above text for us <code><precode>codepen
+cout <<" Thanks for using My Program | GNU/GPL (C) 2017  " <<endl;
+gotoyx(80,9);
+cout <<" ------------------ " <<endl;
+gotoyx(80,10);
+cout <<"| IGN : Newbie C++ |" <<endl;
+gotoyx(80,11);
+cout <<"| NIM : 17615006   |" <<endl;
+gotoyx(80,12);
+cout <<"| -----------------| " <<endl;
+cout <<endl; cout <<endl;
+	system("PAUSE");
 	}
 	
 	return EXIT_SUCCESS;
